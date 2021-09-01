a27bba4dd511bc544dc6c2722abe9089975438f8
mart8ins <mart8ins@gmail.com>
2021-09-01 17:01:18 +0300

    some changes

--- src/layout/components/sideNavigation/SideNavElement.jsx
+++ src/layout/components/sideNavigation/SideNavElement.jsx
@@ -1,8 +1,19 @@
+import React from "react";
 import { NavLink } from "react-router-dom";
 
 
+
 function SideNavElement({ icon }) {
-    return <NavLink className="icon-element" to={`/${icon.type}`} title={icon.type}>{icon.icon}</NavLink>
+    return (
+        <React.Fragment>
+            <NavLink
+                className="icon-element"
+                to={`/${icon.type}`}
+                title={icon.type}>
+                {icon.icon}
+            </NavLink>
+        </React.Fragment>
+    )
 }
 
 export default SideNavElement;
--- src/layout/components/sideNavigation/SideNavigation.jsx
+++ src/layout/components/sideNavigation/SideNavigation.jsx
@@ -1,13 +1,15 @@
+import React from "react";
 import "./sideNavigation.css";
 import SideNavElement from "./SideNavElement";
 import allIcons from "./icons/icons";
 import { v4 as uuidv4 } from 'uuid';
 
-const renderNavIcons = (icon) => {
-    return <SideNavElement key={uuidv4()} icon={icon} />
-}
 
 function SideNavigation() {
+    const renderNavIcons = (icon) => {
+        return <SideNavElement key={uuidv4()} icon={icon} />
+    }
+
     return <div className="sidebar">
         {allIcons.map(renderNavIcons)}
     </div >
