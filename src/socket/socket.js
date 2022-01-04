b8053b7e4c46a6aaac5f65b53f122b9d704d8896
mart8ins <mart8ins@gmail.com>
2022-01-04 12:00:47 +0200

    fixed bug with socketio

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -1,5 +1,7 @@
 import socketClient from "socket.io-client";
-const ENDPOINT = "http://localhost:3002/";
+import globalVariables from "../globalvar";
+// const ENDPOINT = "http://localhost:3002/";
+const ENDPOINT = globalVariables.server;
 const socket = socketClient(ENDPOINT);
 
 export default socket;
