aebc8a28126f09670e9a7b86f0d44081714d9bad
mart8ins <mart8ins@gmail.com>
2022-05-31 16:13:12 +0300

    removed name from header

--- src/layout/header/Header.jsx
+++ src/layout/header/Header.jsx
@@ -1,25 +1,9 @@
 import "./header.css";
-import { useContext } from "react";
-import { Link } from "react-router-dom";
 import AuthNavigation from "../../auth/AuthNavigation";
-import { AuthContext } from "../../context/auth-context";
 
 function Header() {
-    const { authData, setAuthData } = useContext(AuthContext);
-    // to hide auth modal when clicking brand name in header
-    const handleClick = () => {
-        setAuthData({
-            ...authData,
-        });
-    };
-
     return (
         <div className="header-container">
-            <div onClick={handleClick} className="brand-container">
-                <Link to="/">
-                    Martins Meisters <span>CV</span>
-                </Link>
-            </div>
             <AuthNavigation />
         </div>
     );
